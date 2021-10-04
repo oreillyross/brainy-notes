fa9fcd45c42f0688639d7b2bfbc182c293d78ea0
Faktor 10 <oreillyross@gmail.com>
2021-10-04 14:52:42 +0200

    basic routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "./styles.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 // move to its own file later
 function Login() {
@@ -15,10 +16,45 @@ function Dashboard() {
   return <div>This is the logged in screen</div>;
 }
 
+// Move to its own file
+function Home() {
+  return <div> This is the home screen</div>;
+}
+
+// move to its own file
+function Navigation() {
+  return (
+    <div>
+      <Link to="/">Home</Link> | <Link to="/login">Login</Link> |{" "}
+      <Link to="/signup">Signup</Link> | <Link to="/dashboard">Dashboard</Link>{" "}
+    </div>
+  );
+}
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Brainy Notes</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <img alt="Brainy logo" src="./brainy_logo.svg" />
+        <h1>Brainy Notes</h1>
+        <Navigation />
+      </div>
+      <div>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <div>Signup component </div>
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
