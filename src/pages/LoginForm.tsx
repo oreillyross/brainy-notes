8b7b86e002fad9cebe7d1097c4d19aa5377d7a1a
Faktor 10 <oreillyross@gmail.com>
2021-10-12 12:09:36 +0200

    controlled form

--- src/pages/LoginForm.tsx
+++ src/pages/LoginForm.tsx
@@ -2,18 +2,60 @@ import React from "react";
 import "./LoginForm.css";
 
 function LoginForm() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [rememberMe, setRememberMe] = React.useState(false);
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    if (e.currentTarget.name === "email") {
+      setEmail(e.currentTarget.value);
+    }
+    if (e.currentTarget.name === "password") {
+      setPassword(e.currentTarget.value);
+    }
+    if (e.currentTarget.name === "rememberMe") {
+      setRememberMe(e.currentTarget.checked);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    alert(`${email}, ${password}, ${rememberMe}`);
+  };
+
   return (
-    <form className="loginform">
+    <form onSubmit={handleSubmit} className="loginform">
+      <label htmlFor="email">Email</label>
       <input
         className="loginform__input"
         type="email"
+        name="email"
+        id="email"
         placeholder="type your email here..."
+        value={email}
+        onChange={handleChange}
       />
+      <label htmlFor="email">Password</label>
       <input
         className="loginform__input"
-        type="email"
-        placeholder="type your email here..."
+        name="password"
+        id="password"
+        type="password"
+        value={password}
+        onChange={handleChange}
+        placeholder="type your password here..."
       />
+      <label htmlFor="email">Remember me</label>
+      <input
+        type="checkbox"
+        id="rememberMe"
+        checked={rememberMe}
+        onChange={handleChange}
+        name="rememberMe"
+        className="loginform__input"
+      />
+      <button type="submit" className="loginform__submit">
+        Submit
+      </button>
     </form>
   );
 }
