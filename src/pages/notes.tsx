d50dcf4a726cd69495f8e3dd6df3174a26cb1aa0
Ross O'Reilly <oreillyross@gmail.com>
2021-10-21 15:51:12 +0200

    add filter

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -4,13 +4,14 @@ import { Link } from "react-router-dom";
 import { AddNoteButton } from "components/add-note-button";
 
 type Props = {
-  notes: [object];
+  notes: [{ title: string }];
 };
 
 function Notes({ notes }: Props) {
-  const [searchText, setSearchText] = React.useState("")
+  const [searchText, setSearchText] = React.useState("");
+  const [localNotes, setNotes] = React.useState<{title: string}[]>(notes);
 
-  const listNotes = notes ? (
+  const listNotes = localNotes ? (
     notes.map((note: any) => (
       <li className="listnote__item" key={note.id}>
         <Link to={`/notes/${note.id}`}>{note.title}</Link>
@@ -21,27 +22,33 @@ function Notes({ notes }: Props) {
   );
 
   const onSearchChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setSearchText(event.currentTarget.value)
-  }
+    setSearchText(event.currentTarget.value);
+    const filteredNotes: { title: string }[] = localNotes.filter((note) =>
+      note.title.includes(searchText)
+    );
+    setNotes(filteredNotes)
+  };
 
   return (
     <section>
       <div className="searchnotes">
-      <input
-        className="searchnotes__input"
-        name="search"
-        value={searchText}
-        onChange={onSearchChange}
-        id="search"
-        type="text"
-        placeholder="search your notes..."
-      />
-      <button className="searchnotes__button">Search</button>
+        <input
+          className="searchnotes__input"
+          name="search"
+          value={searchText}
+          onChange={onSearchChange}
+          id="search"
+          type="text"
+          placeholder="search your notes..."
+        />
+        <button className="searchnotes__button">Search</button>
       </div>
       <div>
         <ul className="listnote">{listNotes}</ul>
       </div>
-      <div><AddNoteButton/></div>
+      <div>
+        <AddNoteButton />
+      </div>
     </section>
   );
 }
