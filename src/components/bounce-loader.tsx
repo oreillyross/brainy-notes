f582a84b2ea19416eb1ea3c2929b3e36ee5eb463
Faktor 10 <oreillyross@gmail.com>
2021-10-29 10:53:19 +0200

    add new loader

---
+++ src/components/bounce-loader.tsx
@@ -0,0 +1,13 @@
+import "./bounce-loader.css";
+
+const BounceLoader = () => {
+  return (
+    <div className="bounce__loader">
+      <div className="bounce__loader-ball"></div>
+      <div className="bounce__loader-ball"></div>
+      <div className="bounce__loader-ball"></div>
+    </div>
+  );
+};
+
+export { BounceLoader };
--- src/pages/notes-display.tsx
+++ src/pages/notes-display.tsx
@@ -1,19 +1,21 @@
 import { useQuery } from "@apollo/client";
 import * as queries from "../queries/index";
 import { Notes } from "pages/notes";
-import { Loading } from "components/loading-indicator";
-
+// import { Loading } from "components/loading-indicator";
+import { BounceLoader } from "components/bounce-loader";
 const NotesDisplay = () => {
   const { data, loading, error } = useQuery(queries.GET_NOTES);
   if (loading) {
     return (
       <>
-        <Loading />
+        <BounceLoader />
       </>
     );
   }
   if (error) {
     return <> error {error.message}</>;
+
+    // }
   }
 
   if (data) {
