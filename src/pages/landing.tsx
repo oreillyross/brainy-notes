c1fc38afbe9511dc78425a66b98a4636d769cab3
Ross O'Reilly <oreillyross@gmail.com>
2021-11-09 17:06:08 +0100

    formatting

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,13 +1,16 @@
 import { AddNoteButton } from "components/add-note-button";
-import "./landing.scss"
+import "./landing.scss";
 const LandingPage = () => {
   return (
     <>
       <div className="landing">
-        <div className="landing__strap">Welcome to the world's best note taking app. Lets get going by adding
-        some notes...
+        <div className="landing__strap">
+          Welcome to the world's best note taking app. Lets get going by adding
+          some notes...
         </div>
-        <div className="landing__button"><AddNoteButton /></div>
+        <div className="landing__button">
+          <AddNoteButton />
+        </div>
       </div>
       <img className="body__img" alt="people taking notes" src="./people.svg" />
     </>
