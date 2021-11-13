c25800f922c44b51819fd2a91604c1e72259bf1c
Ross O'Reilly <oreillyross@gmail.com>
2021-11-13 20:31:16 +0100

    add

---
+++ src/components/add-note-box.tsx
@@ -0,0 +1,12 @@
+import "./add-note-box.scss";
+
+const AddNoteBox = () => {
+  return (
+    <div>
+      This is a box if you click it will paste the clipboard and then allow you
+      to type a title and save the record
+    </div>
+  );
+};
+
+export {AddNoteBox}
