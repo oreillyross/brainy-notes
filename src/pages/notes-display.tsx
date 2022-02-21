1474a927aef5157db085555a30dd190382b52e6d
Faktor 10 <oreillyross@gmail.com>
2022-02-21 15:05:44 +0100

    display dummy data

--- src/pages/notes-display.tsx
+++ src/pages/notes-display.tsx
@@ -3,8 +3,22 @@ import * as queries from "../queries/index";
 import { Notes } from "pages/notes";
 // import { Loading } from "components/loading-indicator";
 import { BounceLoader } from "components/bounce-loader";
+import * as demoData from "demo/search_results.json";
+
 const NotesDisplay = () => {
+  const { note } = demoData.data;
+
   const { data, loading, error } = useQuery(queries.GET_NOTES);
+  if (process.env.NODE_ENV === "development") {
+    return (
+      <div>
+        Notes
+        {note.map((n) => (
+          <li>{n.title}</li>
+        ))}
+      </div>
+    );
+  }
   if (loading) {
     return (
       <>
