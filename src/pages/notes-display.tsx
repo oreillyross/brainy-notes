209f8db81517f728fe1a82af9066de8ac933b53a
Ross O'Reilly <oreillyross@gmail.com>
2022-03-30 15:32:19 +0200

    add

--- src/pages/notes-display.tsx
+++ src/pages/notes-display.tsx
@@ -1,24 +1,15 @@
 import { useQuery } from "@apollo/client";
 import * as queries from "../queries/index";
 import { Notes } from "pages/notes";
-// import { Loading } from "components/loading-indicator";
+import { Loading } from "components/loading-indicator";
 import { BounceLoader } from "components/bounce-loader";
-import * as demoData from "demo/search_results.json";
+
 
 const NotesDisplay = () => {
-  const { note } = demoData.data;
+ 
 
   const { data, loading, error } = useQuery(queries.GET_NOTES);
-  if (process.env.NODE_ENV === "development") {
-    return (
-      <div>
-        Notes
-        {note.map((n) => (
-          <li>{n.title}</li>
-        ))}
-      </div>
-    );
-  }
+ 
   if (loading) {
     return (
       <>
