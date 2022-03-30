6053b175306cc5a0ca9e7691b2b43e4db9ae0518
Ross O'Reilly <oreillyross@gmail.com>
2022-03-30 15:34:22 +0200

    change

--- src/pages/notes-display.tsx
+++ src/pages/notes-display.tsx
@@ -3,7 +3,6 @@ import * as queries from "../queries/index";
 import { Notes } from "pages/notes";
 import { Loading } from "components/loading-indicator";
 import { BounceLoader } from "components/bounce-loader";
-<<<<<<< HEAD
 
 
 const NotesDisplay = () => {
@@ -11,24 +10,6 @@ const NotesDisplay = () => {
 
   const { data, loading, error } = useQuery(queries.GET_NOTES);
  
-=======
-// import * as demoData from "demo/search_results.json";
-
-const NotesDisplay = () => {
-  // const { note } = demoData.data;
-
-  const { data, loading, error } = useQuery(queries.GET_NOTES);
-  // if (process.env.NODE_ENV === "development") {
-  //   return (
-  //     <div>
-  //       Notes
-  //       {note.map((n) => (
-  //         <li>{n.title}</li>
-  //       ))}
-  //     </div>
-  //   );
-  // }
->>>>>>> 55f27e07263e54fd285eb6d46132f0236e692588
   if (loading) {
     return (
       <>
@@ -39,7 +20,7 @@ const NotesDisplay = () => {
   if (error) {
     return <> error {error.message}</>;
 
-    // }
+    
   }
 
   if (data) {
