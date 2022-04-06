90e4ac926316ff5bc32f2aacdc15dc77cdf154cc
Ross O'Reilly <oreillyross@gmail.com>
2022-04-06 11:58:07 +0200

    add: search button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,1 +1,38 @@
-export {}
+import * as React from "react";
+
+type Props = {
+  onSearch: (s: string) => void
+
+}
+
+
+const SearchBar = ({onSearch}: Props) => {
+    const [searchText, setSearchText] = React.useState("");
+    
+    const onSearchChange = (event: React.FormEvent<HTMLInputElement>) => {
+        setSearchText(event.currentTarget.value)
+        
+        onSearch(searchText)
+      };
+    
+    return (
+      <>
+        <div className="searchnotes">
+        <input
+          className="p-4 border-2 searchnotes__input"
+          name="search"
+          value={searchText}
+          onChange={onSearchChange}
+          id="search"
+          type="text"
+          placeholder="search your notes..."
+        />
+        <button className="p-4 border-2 bg-slate-700 text-gray-300" type="submit">Search</button>
+        
+      </div>
+      </>
+    );
+}
+
+export default SearchBar;
+
