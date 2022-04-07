ab09a9733acb51075f9688222103cb7f8d128625
Faktor 10 <oreillyross@gmail.com>
2022-04-07 17:20:16 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./styles.scss";
+import "./styles.css";
 import * as React from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { EditNoteForm } from "pages/EditNoteForm";
