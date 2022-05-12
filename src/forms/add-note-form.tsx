a8dc0a847423b477b93332aac7fa6e473053f73b
Faktor 10 <oreillyross@gmail.com>
2022-05-12 16:37:49 +0200

    Style the add note form Adding flex flex-col straight away makes the forms look appealing

--- src/forms/add-note-form.tsx
+++ src/forms/add-note-form.tsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 const initialValues: FormValues = {
   title: "",
   description: "",
-  url: "",
+  url: ""
 };
 
 type FormValues = {
@@ -19,12 +19,15 @@ function AddNoteForm() {
   const navigate = useNavigate();
 
   const [addNote] = useMutation(queries.ADD_NOTE, {
-    refetchQueries: [queries.GET_NOTES, "notes"],
+    refetchQueries: [queries.GET_NOTES, "notes"]
   });
 
   return (
-    <div>
-      <h2> Add a note</h2>
+    <div className="max-w-md mx-auto border ">
+      <h2 className="text-center bg-slate-100 text-slate-800 font-bold py-2 rounded border">
+        {" "}
+        Add a note
+      </h2>
       <Formik
         initialValues={initialValues}
         onSubmit={(values) => {
@@ -35,26 +38,42 @@ function AddNoteForm() {
       >
         {(props) => {
           return (
-            <Form className="addnoteform">
-              <label htmlFor="title">Title</label>
+            <Form className="container flex flex-col">
+              <label
+                className="px-2 font-bold text-slate-700 pt-2"
+                htmlFor="title"
+              >
+                Title
+              </label>
               <Field
-                className="placeholder:text-slate-600 placeholder:italic text-green-700 p-1 m-1"
+                className=" border placeholder:text-slate-600 placeholder:italic text-green-700 p-1 m-1"
                 id="title"
                 type="text"
                 name="title"
                 placeholder="type a title"
               />
-              <label htmlFor="description">Description</label>
+              <label
+                className="px-2 font-bold text-slate-700 pt-2"
+                htmlFor="description"
+              >
+                Description
+              </label>
               <Field
-                className="placeholder:text-slate-600 placeholder:italic text-green-700 p-1 m-1"
+                className="border placeholder:text-slate-600 placeholder:italic text-green-700 p-1 m-1"
                 id="description"
                 as="textarea"
                 name="description"
+                rows="8"
                 placeholder="give a meaningful description"
               />
-              <label htmlFor="url">Url</label>
+              <label
+                className="px-2 font-bold text-slate-700 pt-2"
+                htmlFor="url"
+              >
+                Url
+              </label>
               <Field
-                className="placeholder:text-slate-600 placeholder:italic text-green-700 p-1 m-1"
+                className="border placeholder:text-slate-600 placeholder:italic text-green-700 p-1 m-1"
                 id="url"
                 type="text"
                 name="url"
@@ -62,7 +81,7 @@ function AddNoteForm() {
               />
               <div className="text-center ">
                 <button
-                  className=" rounded-md bg-orange-800 text-white p-2 w-48"
+                  className=" focus:text-black mt-4 rounded-md bg-orange-800 text-white p-2 w-48"
                   type="submit"
                 >
                   Submit
