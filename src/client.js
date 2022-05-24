84c756771118911e330acb1527ca5d989c7caa15
Ross O'Reilly <oreillyross@gmail.com>
2022-05-24 13:13:08 +0200

    fix supabase bug

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.REACT_CLIENT_SUPABASE_URL,
-  process.env.REACT_CLIENT_SUPABASE_ANON_KEY
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
 );
--- src/pages/notes-display.tsx
+++ src/pages/notes-display.tsx
@@ -7,9 +7,14 @@ import SearchBar from "../components/SearchBar";
 import { BounceLoader } from "components/bounce-loader";
 import { supabase } from "../client";
 import { useEffect } from "react";
+import { PostgrestResponse } from "@supabase/supabase-js";
+
+type TNote = {
+  title: string;
+}
 
 const NotesDisplay = () => {
-  const [notes, setNotes] = React.useState([]);
+  const [notes, setNotes] = React.useState<TNote[]>([]);
   const [note, setNote] = React.useState({
     title: "",
     description: "",
@@ -21,10 +26,17 @@ const NotesDisplay = () => {
     fetchNotes();
   }, []);
 
+  type JSONResp = {
+    data?: any[]
+  }
+
   async function fetchNotes() {
-    const { data } = await supabase.from("notes").select();
+    const {data} = await supabase.from("notes").select();
+    
     console.log(data);
-    setNotes([]);
+    //@ts-ignore
+    setNotes(data)
+    
   }
 
   async function createNote() {
@@ -54,7 +66,13 @@ const NotesDisplay = () => {
           value={title}
           onChange={(e) => setNote({ ...note, url: e.target.value })}
         />
-        <button onClick></button>
+        <button>Add Note</button>
+        <div className="m-24">
+           Some data
+           {notes.map((note) => {
+             <li>{note.title}</li>
+           })}
+        </div>
       </div>
     </>
   );
