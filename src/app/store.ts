b2813a01f002ff4b020965ce355ce7882d90b8ac
Faktor 10 <oreillyross@gmail.com>
2022-05-31 13:00:42 +0200

    initial commit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,16 @@
-import {configureStore, getDefaultMiddleware} from "@reduxjs/toolkit"
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import counterReducer from "../components/Counter/counter-slice";
 import { apiSlice } from "../features/dogs/dogs-api-slice";
 
-
 export const store = configureStore({
-	reducer: {
-		counter: counterReducer,
-		[apiSlice.reducerPath]: apiSlice.reducer,
-	},
-	middleware: (getDefaultMiddleware) => {
-		return getDefaultMiddleware().concat(apiSlice.middleware)
-	}
-})
+  reducer: {
+    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware);
+  }
+});
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-
--- src/client.js
+++ src/client.js
@@ -1,13 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = process.env.REACT_APP_SUPABASE_URL;
-const anon_key = process.env.REACT_APP_SUPABASE_ANON_KEY;
-let supabase = {};
-if (url && anon_key) {
-  const supabase = createClient(
-    process.env.REACT_APP_SUPABASE_URL,
-    process.env.REACT_APP_SUPABASE_ANON_KEY
-  );
-}
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
+);
 
 export { supabase };
