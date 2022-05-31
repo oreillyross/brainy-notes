e3c34ff22d28ae23565201619475d2a822f352c5
Faktor 10 <oreillyross@gmail.com>
2022-05-31 13:53:20 +0200

    back to square one Next step is apply redux, and introduce the redux-thunk for async AJAX calls to supabase backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,21 @@
+import { Link } from "react-router-dom";
+import Navigation from "features/navigation/navigation";
 
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 export default function App() {
   return (
     <>
-        <div className="container relative px-5 flex">
-          <Link to="/">
-            <img className="p-6" alt="Brainy logo" src="/brainy_logo.svg" />
-          </Link>
-          <div className="app__login">{/* <UserBar /> */}</div>
+      <div className="container relative px-5 flex">
+        <Link to="/">
+          <img className="p-6" alt="Brainy logo" src="img/brainy_logo.svg" />
+        </Link>
+        <div className="app__login">{/* <UserBar /> */}</div>
 
-          <h1 className="m-auto text-6xl font-bold text-amber-900">
-            Brainy Notes
-          </h1>
-        </div>
-        <Navigation />
-        <div className="">
-          <Routes>
-            <Route path="/login" element={<Login />} />
-
-            <Route path="/logout" element={<Logout />} />
-
-            <Route path="/addnote" element={<AddNoteForm />} />
-
-            <Route path="/edit" element={<EditNoteForm />} />
-
-            <Route path="/note/:id" element={<NoteDisplay />} />
-
-            <Route path="/notes" element={<NotesDisplay />} />
-
-            <Route path="/notecards" element={<NotesDisplay />} />
-
-            <Route path="/about" element={<Counter />} />
-
-            <Route path="/" element={<LandingPage />} />
-          </Routes>
-        </div>
-      </Router>
-    </ApolloProvider>
+        <h1 className="m-auto text-6xl font-bold text-amber-900">
+          Brainy Notes
+        </h1>
+      </div>
+      <Navigation />
+      <div>The home page...</div>
+    </>
   );
 }
