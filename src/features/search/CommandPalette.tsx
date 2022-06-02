87eaca87870e8e69a108affd416b6627e5bb41a3
Ross O'Reilly <oreillyross@gmail.com>
2022-06-02 21:21:50 +0200

    updated with tailwindcss

--- src/features/search/CommandPalette.tsx
+++ src/features/search/CommandPalette.tsx
@@ -1,14 +1,119 @@
-import type { Note } from "../../types";
+import { Combobox, Dialog, Transition } from "@headlessui/react";
+import { useState, useEffect, Fragment } from "react";
+import { BookmarkIcon, SearchIcon } from "@heroicons/react/outline";
+import { useNavigate } from "react-router-dom";
 
-type Props = {
-  notes: Note[];
-};
-export default function CommandPalette({ notes }: Props) {
+export default function CommandPalette({ notes }) {
+  const [isOpen, setIsOpen] = useState(true);
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    function onKeyDown(event) {
+      if (event.key === "%" && (event.metaKey || event.ctrlKey)) {
+        setIsOpen(!isOpen);
+      }
+    }
+
+    window.addEventListener("keydown", onKeyDown);
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [isOpen]);
+
+  const filteredNotes = query
+    ? notes.filter((note) =>
+        note.title.toLowerCase().includes(query.toLowerCase())
+      )
+    : [];
+
   return (
-    <div className="fixed inset-x-0 top-0 z-10 bg-purple-200">
-      <p className="py4 text-center text-purple-400">
-        Let build a Command palette
-      </p>
-    </div>
+    <Transition.Root
+      show={isOpen}
+      as={Fragment}
+      afterLeave={() => {
+        setQuery("");
+      }}
+    >
+      <Dialog
+        onClose={setIsOpen}
+        className="fixed inset-0 p-4 pt-[25vh] overflow-y-auto"
+      >
+        <Transition.Child
+          enter="duration-300 ease-out"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="duration-200 ease-in"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Dialog.Overlay className="fixed inset-0 bg-gray-500/75" />
+        </Transition.Child>
+        <Transition.Child
+          enter="duration-300 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-200 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Combobox
+            as="div"
+            className="overflow-hidden p-3 relative ring-1 ring-black/5 shadow-2xl rounded-xl max-w-xl mx-auto bg-white divide-y divide-gray-100"
+            onChange={(value) => {
+              setIsOpen(false);
+              navigate(`/notes/${value.id}`);
+            }}
+          >
+            <div className="flex items-center ">
+              <SearchIcon className="h-6 w-6 text-gray-500 mx-2" />
+              <Combobox.Input
+                onChange={(event) => {
+                  setQuery(event.target.value);
+                }}
+                className="w-full p-2 bg-transparent border-0 focus:outline-none focus:ring-0 text-sm text-xl text-gray-800"
+                placeholder="Search..."
+              />
+            </div>
+            {filteredNotes.length > 0 && (
+              <Combobox.Options
+                static
+                className="py-4 text-sm max-h-96 overflow-y-auto"
+              >
+                {filteredNotes.map((note) => (
+                  <Combobox.Option key={note.id} value={note}>
+                    {({ active }) => (
+                      <div
+                        className={`flex items-center space-x-1 px-4 py-2 ${
+                          active ? "bg-indigo-600" : "bg-white"
+                        }`}
+                      >
+                        <span
+                          className={`font-medium text-gray-900 px-4 py-2 ${
+                            active ? "text-white" : "text-gray-600"
+                          }`}
+                        >
+                          {note.title}
+                        </span>
+                        <span>
+                          <BookmarkIcon
+                            className={`h-4 w-4  ${
+                              active ? "text-grey-100" : "text-gray-400"
+                            }`}
+                          />
+                        </span>
+                      </div>
+                    )}
+                  </Combobox.Option>
+                ))}
+              </Combobox.Options>
+            )}
+            {query && filteredNotes.length === 0 && (
+              <p className="p-4 text-sm text-gray-500">No notes found</p>
+            )}
+          </Combobox>
+        </Transition.Child>
+      </Dialog>
+    </Transition.Root>
   );
 }
