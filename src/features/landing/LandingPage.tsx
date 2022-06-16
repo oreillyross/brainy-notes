b438bd8123cf456d3d623ea418f7c2b4b612297a
Faktor 10 <oreillyross@gmail.com>
2022-06-16 15:16:53 +0200

    responsive design for xl screens

--- src/features/landing/LandingPage.tsx
+++ src/features/landing/LandingPage.tsx
@@ -9,22 +9,22 @@ function LandingPage() {
           <br className="hidden lg:inline 2xl:hidden" />
           and <span className="text-indigo-500">retrieve</span> them anytime
         </h1>
-        <h3 className="hidden 2xl:block 2xl:text-center text-2xl pt-4 md:pt-12 px-4">
+        <h3 className="text-gray-800 hidden 2xl:block 2xl:text-center text-2xl pt-4 md:pt-12 px-4">
           Brainy notes helps you create learning notes with ease, without
           disrupting you from your workflow, and just as easily retrieve them.
         </h3>
         <div className="2xl:pl-48 2xl:pt-24 md:flex 2xl:text-center 2xl:align-center">
-          <div className="2xl:w-2/3 2xl:p-8">
+          <div className="2xl:w-2/3 ">
             <img
               alt="people working"
-              className="bg-pink-100 pt-6 md:h-72 2xl:h-96 object-contain"
+              className="pt-6 md:h-72 2xl:h-96 object-fit"
               src="img/people.svg"
             />
           </div>
-          <div className="hidden 2xl:block bg-red-200 2xl:w-full">
+          <div className="hidden 2xl:block 2xl:w-full">
             <Splash />
           </div>
-          <div className="bg-green-600 md:p-8">
+          <div className="md:p-8">
             <h3 className="2xl:hidden text-2xl pt-4 md:pt-12 px-4">
               Brainy notes helps you create learning notes with ease, without
               disrupting you from your workflow, and just as easily retrieve
