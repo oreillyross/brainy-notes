03c1b349c8cca86a689f7f7748ea70f79ba68b01
Ross O'Reilly <oreillyross@gmail.com>
2022-06-20 17:44:31 +0200

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Link, Routes, Route } from "react-router-dom";
 import Navigation from "features/navigation/navigation";
 import { LandingPage } from "features/landing/LandingPage";
-import NotesList from "features/notes/notesList";
+import NotesList from "features/notes/NotesList";
 import CommandPalette from "features/search/CommandPalette";
 import { useAppSelector } from "app/hooks";
 import Note from "features/notes/Note";
 import AddNoteForm from "features/notes/add-note-form";
 import EditNoteForm from "features/notes/edit.note.form";
+import About from "features/profile/About"
 
 export default function App() {
   
@@ -33,6 +34,7 @@ export default function App() {
           <Route path="/notes/new" element={<AddNoteForm />} />
           <Route path="/note/:id" element={<Note/>}/>
           <Route path="/note/edit/:id" element={<EditNoteForm />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </div>
     </>
