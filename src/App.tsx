9f71896a19bd131372e6d0d4cffdb916293ef7f3
Ross O'Reilly <oreillyross@gmail.com>
2022-06-22 10:04:41 +0200

    add hamburger icon

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { useAppSelector } from "app/hooks";
 import Note from "features/notes/Note";
 import AddNoteForm from "features/notes/add-note-form";
 import EditNoteForm from "features/notes/edit.note.form";
-import About from "features/profile/About"
+import About from "features/profile/About";
 import HamburgerMenu from "features/navigation/HamburgerMenu";
 
 export default function App() {
@@ -20,7 +20,14 @@ export default function App() {
         <div className="flex ">
           <div className="flex-auto">
             <Link to="/">
-              <img className="" alt="Brainy logo" src="img/brainy_logo.jpg" />
+              <img
+                className="inline-block"
+                alt="Brainy logo"
+                src="img/brainy_logo.svg"
+              />
+              <div className="inline-block text-6xl py-12 px-8 text-red-700 font-bold">
+              Brainy Notes
+                </div>
             </Link>
           </div>
 
--- src/features/navigation/HamburgerMenu.tsx
+++ src/features/navigation/HamburgerMenu.tsx
@@ -9,7 +9,7 @@ function HamburgerMenu() {
   };
   if (!isOpen) {
     return (
-      <div className="flex flex-col justify-center pr-8">
+      <div className="flex flex-col justify-center pr-8 ">
         <button onClick={openMenu}>
           <div className="bg-slate-900 h-2 w-12 mt-2"></div>
           <div className="bg-slate-900 h-2 w-12 mt-2"></div>
@@ -22,14 +22,23 @@ function HamburgerMenu() {
       <>
         <div className="flex flex-col justify-center pr-8">
           <button onClick={openMenu}>
-            <div className="bg-slate-900 h-2 w-12 mt-2"></div>
+            <div className="transition duration-200 ease-in-out pt-2 bg-slate-900 h-2 w-12 mt-2 rotate-45"></div>
+            <div className=" transition duration-200 ease-in-out -translate-y-[14px] bg-slate-900 h-2 w-12 mt-2 -rotate-45"></div>
           </button>
         </div>
-        <div>
-          <ul>Notes</ul>
-          <ul>Settings</ul>
-          <ul>About</ul>
-          <ul>Log In</ul>
+        <div className="absolute top-10 right-28 bg-slate-800 w-32 h-64">
+          <ul className="text-2xl p-2 text-white font-semibold hover:underline">
+            <a href="/notes">Notes</a>
+          </ul>
+          <ul className="text-2xl p-2 text-white font-semibold hover:underline">
+            <a href="/">Settings</a>
+          </ul>
+          <ul className="text-2xl p-2 text-white font-semibold hover:underline">
+            <a href="/">About</a>
+          </ul>
+          <ul className="text-2xl p-2 text-white font-semibold hover:underline">
+            <a href="/">Log In</a>
+          </ul>
         </div>
       </>
     );
