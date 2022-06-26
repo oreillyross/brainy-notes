7d295028a8ac5fcade9675f7b63c6581c495b839
Ross O'Reilly <oreillyross@gmail.com>
2022-06-26 14:32:26 +0200

    changes

---
+++ src/features/notes/Note.test.tsx
@@ -0,0 +1,16 @@
+import { screen, render } from "@testing-library/react";
+import Note from "./Note";
+import { notes } from "api/mocks/staticNotes";
+import { BrowserRouter as Router } from "react-router-dom";
+
+describe("<Note/>", () => {
+  it("Renders a generic note", () => {
+    const { debug, getByRole } = render(
+      <Router>
+        <Note note={notes[0]} />
+      </Router>
+    );
+
+    expect(screen.getByRole("heading")).toBeDefined();
+  });
+});
