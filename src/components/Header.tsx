6e0308c976dedbd796fc4d0c02e6033d76c03dd1
Ross O'Reilly <oreillyross@gmail.com>
2022-10-16 21:12:47 +0200

    change header location

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,11 +14,11 @@ export default function Header() {
       <Link to="/">
         <img className="m-8" alt="Brainy logo" src="img/brainy_logo.svg" />
       </Link>
-      <div className="m-auto justify-self-center md:text-7xl text-6xl py-12 px-8 text-green-700 font-bold">
+      <div className="mx-auto md:text-7xl text-6xl py-12  text-green-700 font-bold">
         Brainy Notes
       </div>
       {user ? (
-        <span className="max-w-sm mx-auto">
+        <span className="max-w-sm pr-8">
           <button
             onClick={handlelogout}
             className="border p-6 m-6 rounded bg-green-800 text-white min-w-full text-xl font-semibold"
